e54a7680dbe33129f7898d7a68a8c13a2ce4b26b
githubUser435543 <jethanwesson08@gmail.com>
2024-09-27 16:59:06 -0400

    removed unneeded lines

--- graph_theory/dijkstras.py
+++ graph_theory/dijkstras.py
@@ -4,16 +4,11 @@ def dijkstras(g, start, end):
     pq = []
     heappush(pq, (0, start))
     visited = [-1 for n in g]
-    #visited[start] = 0
-
     while (len(pq) > 0):
-        #print(visited) # debug
         cW , cN = heappop(pq)
         if visited[cN] != -1: continue
-        #print(cN) # debug
         visited[cN] = cW 
         if (cN == end): return cW 
-
         for eW, eN in g[cN]:
             if visited[eN] != -1: continue
             heappush(pq, (cW + eW, eN))
