ec3325396b078b1fd5c370a17634fc27498d43cb
Jacob <ethan@DEBIAN.debian>
2024-10-09 20:14:34 -0400

    added bsmin

--- misc/binary_search.py
+++ misc/binary_search.py
@@ -1,6 +1,17 @@
 #!/bin/python3
 
-def bsMin(): pass
+   def bsmin(lo, hi, f):
+  1     if not f(hi): return "oof"
+  2     assert hi >= lo
+  3     while lo < hi:
+  4         mid = lo + (hi - lo) // 2
+  5
+  6         if f(mid):
+  7             hi = mid
+  8         else:
+  9             lo = mid + 1
+ 10
+ 11     return lo
 
 def bsMax(): pass
 
