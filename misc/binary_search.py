0bf84f754d7d1f0bb53dafa80cdbc5b0b24be8af
Jacob <ethan@DEBIAN.debian>
2024-10-19 18:25:35 -0400

    fixed a stupid bug, not sure if it works yet

--- misc/binary_search.py
+++ misc/binary_search.py
@@ -1,17 +1,16 @@
 #!/bin/python3
 
-   def bsmin(lo, hi, f):
-  2     assert hi >= lo
-  1     if not f(hi): return "404"
-  3     while lo < hi:
-  4         mid = lo + (hi - lo) // 2
-  5
-  6         if f(mid):
-  7             hi = mid
-  8         else:
-  9             lo = mid + 1
- 10
- 11     return lo
+def bsmin(lo, hi, f):
+    assert hi >= lo
+    if not f(hi): return "404"
+    while lo < hi:
+        mid = lo + (hi - lo) // 2
+
+    if f(mid):
+        hi = mid
+    else:
+        lo = mid + 1
+        return lo
 
 def bsMax(lo, hi, f):
     assert hi >= lo
