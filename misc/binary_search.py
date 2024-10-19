14a046ba7849a6bdc02ac110fd8d15e983e837a8
Jacob <ethan@DEBIAN.debian>
2024-10-19 17:47:37 -0400

    finished binary searches

--- misc/binary_search.py
+++ misc/binary_search.py
@@ -1,8 +1,8 @@
 #!/bin/python3
 
    def bsmin(lo, hi, f):
-  1     if not f(hi): return "oof"
   2     assert hi >= lo
+  1     if not f(hi): return "404"
   3     while lo < hi:
   4         mid = lo + (hi - lo) // 2
   5
@@ -13,7 +13,30 @@
  10
  11     return lo
 
-def bsMax(): pass
+def bsMax(lo, hi, f):
+    assert hi >= lo
+    if not f(lo): return "404"
+    while lo < hi:
+        mid = lo + (hi - lo) // 2
 
-def bsArr(): pass
+        if f(mid):
+            lo = mid
+        else:
+            hi = mid - 1
+    return hi
 
+def bsArr(arr, val):
+    lo = 0
+    hi = len(arr)
+    
+    while lo < hi:
+        mid = lo + (lo + hi) // 2
+
+        if arr[mid] < val:
+            lo = mid
+        elif arr[mid] > val:
+            hi = mid
+        else:
+            return arr[mid]
+
+    return "404"
